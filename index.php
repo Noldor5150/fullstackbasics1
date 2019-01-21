45f7e26ad681d3f406fc8ff433aa4a70c50ecdeb
Noldor5150 <ruikis.paulius@gmail.com>
2019-01-21 12:37:05 +0200

    rand funkcijo, kintamieji, syntaxes updeitas 5 uzduotis

--- index.php
+++ index.php
@@ -8,8 +8,8 @@ $randaom_color3 = rand(0,255);
     <head>
         <title> PHP lydes ir <?php print date("Y", strtotime('+' . rand(0, 10) . 'year')); ?> </title>
     </head>
-    <body style="background-color:rgb(<?php print $randaom_color1 . ',' . $randaom_color2 . ',' . $randaom_color3; ?>)">
+    <body style="background-color:rgb(<?php print "$randaom_color1, $randaom_color2, $randaom_color3" ?>)">
         <h1 style="font-size:<?php print rand(10,100);?>">As keiciu savo dydi <?php print rand(1, 5); ?> vaiku </h1>
-        <p style="background-color:rgb(<?php print $randaom_color2 . ',' . $randaom_color1 . ',' . $randaom_color3; ?>)"> as keiciu spalva </p>
+        <p style="background-color:rgb(<?php print "$randaom_color2, $randaom_color1,$randaom_color3" ?>)"> as keiciu spalva </p>
     </body>
 </html>
