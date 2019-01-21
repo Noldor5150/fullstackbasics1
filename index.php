adf5fec274967ff7d2d2c47e2b048881abe594c4
Noldor5150 <ruikis.paulius@gmail.com>
2019-01-21 12:31:28 +0200

    rand funkcijo, kintamieji 5 uzduotis

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
-<?php Date_default_timezone_set('Europe/Vilnius'); ?>
+<?php 
+$randaom_color1 = rand(0,255);
+$randaom_color2 = rand(0,255);
+$randaom_color3 = rand(0,255);
+?>
 
 <html>
     <head>
-        <title> PHP lydes ir <?php print date("Y", strtotime ('+'.rand(0,10).'year'));?> </title>
+        <title> PHP lydes ir <?php print date("Y", strtotime('+' . rand(0, 10) . 'year')); ?> </title>
     </head>
-    <body>
-        
-        <h1>As - galbut turesiu <?php print rand(1,5);?> vaiku </h1>
-            <p>Trumpas nebus preziku <?php print date("Y-m-d", strtotime ('+'.rand(0,10).'year'));?> </p>
-        
-
+    <body style="background-color:rgb(<?php print $randaom_color1 . ',' . $randaom_color2 . ',' . $randaom_color3; ?>)">
+        <h1 style="font-size:<?php print rand(10,100);?>">As keiciu savo dydi <?php print rand(1, 5); ?> vaiku </h1>
+        <p style="background-color:rgb(<?php print $randaom_color2 . ',' . $randaom_color1 . ',' . $randaom_color3; ?>)"> as keiciu spalva </p>
     </body>
 </html>
