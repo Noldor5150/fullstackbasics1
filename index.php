d659cd1b53146dc06efc1a6302bd1c424690e95d
Noldor5150 <ruikis.paulius@gmail.com>
2019-01-21 11:05:59 +0200

    2 Uzduotis

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <html>
     <head>
-        <title><<?php print 'As, ' . date("l") . " ir PHP" ?></title>
+        <<title>PHP lydes ir <?php print date("Y-m-d", strtotime("+1 day")); ?></title>
     </head>
     <body>
-        <h1>Paulius - HTML ir PHP asas jau nuo <?php print date('Y') ?> metu</h1>
-        <p>Viska prasidejo <?php print date('m') ?> menesio, <?php print date('d') ?> diena! </p>
+        <h1>Paulius - PHP su manim buvo <?php print date('Y-m-d H:i:s', strtotime('-1 hour')); ?></h1>
+        <p><?php print date("Y", strtotime("+1 year")); ?> ne uz kalnu</p>
     </body>
 </html>
-
