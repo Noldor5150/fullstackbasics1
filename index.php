23b912f9d794b1b889ba09935d933b9453d1d653
Noldor5150 <ruikis.paulius@gmail.com>
2019-01-21 11:59:37 +0200

    strtotime ir rand fjos

--- index.php
+++ index.php
@@ -2,20 +2,13 @@
 
 <html>
     <head>
-        <title>Komentarai</title>
+        <title><title> PHP lydes ir <?php print date("Y", strtotime ('+'.rand(0,10).'year'));?>  </title></title>
     </head>
     <body>
-        <?php 
-        /*
-        <div>
-            <h1>As pradedu</h1>
-            <h2>O as papildau</h2>
-        </div>
-        */
-        ?>
-        <div>
-            <p>As esu ta pana, kuri kalba ir kalba ir kalba...</p>
-        </div>
+        
+        <h1>As - galbut turesiu <?php print rand(1,5);?> vaiku </h1>
+            <p>Trumpas nebus preziku <?php print date("Y-m-d", strtotime ('+'.rand(0,10).'year'));?> </p>
+        
 
     </body>
 </html>
