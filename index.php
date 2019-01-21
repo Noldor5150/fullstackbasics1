0bef1943d191cadf3faaa4c14c751ed7e0058d85
Noldor5150 <ruikis.paulius@gmail.com>
2019-01-21 10:33:23 +0200

    pirma uzduotis

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>As, pirmadienis ir PHP</title>
+    </head>
+    <body>
+        <h1>Paulius - HTML ir PHP asas jau nuo <?php print date('Y') ?> metu</h1>
+        <p>Viska prasidejo <?php print date('m') ?> menesio, <?php print date('d') ?> diena! </p>
+    </body>
+</html>
+
