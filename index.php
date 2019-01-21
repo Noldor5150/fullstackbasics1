31d9cf8d5123634880fc001f871c01c381233dbe
Noldor5150 <ruikis.paulius@gmail.com>
2019-01-21 12:57:21 +0200

    6 uzduotis

--- index.php
+++ index.php
@@ -5,10 +5,12 @@ $randaom_color3 = rand(0,255);
 ?>
 
 <html>
+    
     <head>
-        <title> PHP lydes ir <?php print date("Y", strtotime('+' . rand(0, 10) . 'year')); ?> </title>
+        <title>  </title>
+        <link rel="stylesheet" href="css/main.css">
     </head>
-    <body style="background-color:rgb(<?php print "$randaom_color1, $randaom_color2, $randaom_color3" ?>)">
+    <body class="background-<?php print rand(0,5);?>">
         <h1 style="font-size:<?php print rand(10,100);?>">As keiciu savo dydi <?php print rand(1, 5); ?> vaiku </h1>
         <p style="background-color:rgb(<?php print "$randaom_color2, $randaom_color1,$randaom_color3" ?>)"> as keiciu spalva </p>
     </body>
