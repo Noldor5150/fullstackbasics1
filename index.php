b369f723ab523dec873cde1feaf428121a100aee
Noldor5150 <ruikis.paulius@gmail.com>
2019-01-21 11:50:28 +0200

    php  pataisyti komentarai, kad nesimatytu chrome

--- index.php
+++ index.php
@@ -5,14 +5,17 @@
         <title>Komentarai</title>
     </head>
     <body>
-        <!-- nu va blogas oi blogas tas divas -->
+        <?php 
+        /*
         <div>
             <h1>As pradedu</h1>
             <h2>O as papildau</h2>
         </div>
-        <!--  <div>
-              <p>As esu ta pana, kuri kalba ir kalba ir kalba...</p>
-          </div>
-        //-->
+        */
+        ?>
+        <div>
+            <p>As esu ta pana, kuri kalba ir kalba ir kalba...</p>
+        </div>
+
     </body>
 </html>
