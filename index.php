c07e53b2fd420527fd048a9e33f68ed83560ff1c
Noldor5150 <ruikis.paulius@gmail.com>
2019-01-22 12:39:22 +0200

    4 uzduotis su pataisymais

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
+Date_default_timezone_set('Europe/Vilnius');
 $siusklines_turis = 40;
 $dienos_siuksliu_turis = rand(0, 10);
 $max_kaupo_turis = rand(1, 3);
-$dienos = ($siusklines_turis + $max_kaupo_turis)/$dienos_siuksliu_turis ;
+$dienos = ($siusklines_turis + $max_kaupo_turis) / $dienos_siuksliu_turis;
+$data = date('d', strtotime("+$dienos days"));
 ?>
 <html>
     <head>
         <title>3 taskas</title>
     </head>
     <body>
-        <h1>Skolos skaiciuokle</h1>
-        <h2>Jei paemei <?php print (round ($dienos)); ?> dienu pirksiu geles</h2>
-        
+        <h1>siukles</h1>
+        <h2> <?php print ceil($data); ?> pirksiu geles</h2>
     </body>
 </html>
