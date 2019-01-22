ed33e842470eec54a07b76ddefcd172905303fc5
Noldor5150 <ruikis.paulius@gmail.com>
2019-01-22 11:57:40 +0200

    3uzduotis, su ataisymais

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-$sudas1 = rand(1, 100);
-$sudas2 = rand(101, 200);
-$sudas3 = rand(201, 300);
+$paskola = rand(1, 100);
+$skolos_grazinimas1 = rand(101, 200);
+$skolos_grazinimas2 = rand(201, 300);
 ?>
 <html>
     <head>
@@ -9,8 +9,8 @@ $sudas3 = rand(201, 300);
     </head>
     <body>
         <h1>Skolos skaiciuokle</h1>
-        <h2>Jei paemei <?php print $sudas1; ?> jievru</h2>
-        <h2>Su dviem kabanciais grazinsi <?php print $sudas2; ?></h2>
-        <h2>Su vienu kabanciu grazinsi <?php print $sudas3; ?></h2>
+        <h2>Jei paemei <?php print $paskola; ?> jievru</h2>
+        <h2>Su dviem kabanciais grazinsi <?php print $skolos_grazinimas1; ?></h2>
+        <h2>Su vienu kabanciu grazinsi <?php print $skolos_grazinimas2; ?></h2>
     </body>
 </html>
