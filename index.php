8b587b9f8df8c18007b35406ddf33ba601b7195c
Noldor5150 <ruikis.paulius@gmail.com>
2019-01-23 11:13:46 +0200

    for ciklai 2 uzduotis su pataiymasi

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $po_alaus = rand(1, 5);
 $dar_alaus = 0;
 $kablys = 1;
-for ($i = 1; $i < $po_alaus; $i++) {
+for ($i = 0; $i <= $po_alaus; $i++) {
     $dar_alaus += floor($po_alaus / 2) + $kablys;
 }
 ?>
