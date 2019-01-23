f310adf45f1bc1949a75cf1c6c4bd85c303560c5
Noldor5150 <ruikis.paulius@gmail.com>
2019-01-23 16:05:00 +0200

    for ciklas 6 uzduotis su pataisymais

--- index.php
+++ index.php
@@ -1,23 +1,32 @@
 <?php
-$months = 12;
-$kisene = 1000;
-$alga = 700;
-for ($i = 1; $i <= $months; $i++) {
-    $islaidos = rand(400, 1200);
-    $kisene += $alga - $islaidos;
-    if ($kisene <= 0) {
-        $message = "Bloga prognozė: $i mėnesį gali baigtis piginai! Atsargiai!";
-        break;
+$days = 365;
+$pakelio_kaina = 3.75;
+$ciza = $pakelio_kaina / 20;
+$cizu_kiekis = 0;
+$cizu_kiekis_pirm_ketv = 0;
+for ($i = 1; $i <= $days; $i++) {
+    $savaites_diena = date('N', strtotime("+ $i day"));
+    $ciza_mon_thu = rand(10, 20);
+    $ciza_fri = rand(20, 30);
+    $ciza_sat_sun = rand(5, 10);
+    if ($savaites_diena <= 4) {
+        $cizu_kiekis += $ciza_mon_thu;
+        $cizu_kiekis_pirm_ketv += $ciza_mon_thu;
+    } elseif ($savaites_diena == 5) {
+        $cizu_kiekis += $ciza_fri;
     } else {
-        $message = "Likutis po $i mėn. yra $kisene Eur.";
+        $cizu_kiekis += $ciza_sat_sun;
     }
 }
+$viso_islaidos = $cizu_kiekis * $ciza;
+$mon_thu_kaina =  $cizu_kiekis_pirm_ketv * $ciza;
 ?>
 <html>
     <head>
-        <title>Kišenė</title>
+        <title>Fucking rookymas</title>
     </head>
     <body>
-        <h2><?php print $message; ?></h2>
+        <h2>Surookiau cyzu per metus <?php print $cizu_kiekis; ?> isleidau <?php print $viso_islaidos; ?> jevru</h2>
+        <h2>Nerukydamas pirmadieniais - ketvirtadieniais sutaupyciau <?php print $mon_thu_kaina; ?> jevru</h2>
     </body>
 </html>
