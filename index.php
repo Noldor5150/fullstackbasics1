d30443488c4baf1e6b69a2c4a78a026b9dac80a0
Noldor5150 <ruikis.paulius@gmail.com>
2019-01-24 13:28:08 +0200

    array 2 uzduotis

--- index.php
+++ index.php
@@ -1,9 +1,29 @@
 <?php
-$array = [
-    'kiausiniai' => 2,
-    'mazas_baklazanas' => 1,
-    'didelis_grietines_indelis' => 1,
-     
+$people = [
+    '0' => [
+        'vardas_pavarde' => 'Petras Pizdys',
+        'daiktai' => [
+            'telefonas' => [
+               'daikto_busena' => 'prapistas'
+            ]
+        ]
+    ],
+    '1' => [
+        'vardas_pavarde' => 'Tomas Ablomas',
+        'daiktai' => [
+            'buljonas' => [
+                'daikto_busena' => 'naudojamas'
+            ]
+        ]
+    ],
+    '2' => [
+        'vardas_pavarde' => 'Ana Shitkova',
+        'daiktai' => [
+            'rulonas' => [
+                'daikto_busena' => 'pasibaiges'
+            ],
+        ]
+    ]
 ];
-
-var_dump($array);
+var_dump($people);
+?>
