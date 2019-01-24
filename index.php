2386fd559f8c0b843fbeec4ad541d1aa7e7fbec3
Noldor5150 <ruikis.paulius@gmail.com>
2019-01-24 13:41:39 +0200

    array 2 uzduotis update

--- index.php
+++ index.php
@@ -1,26 +1,30 @@
 <?php
+
 $people = [
     '0' => [
         'vardas_pavarde' => 'Petras Pizdys',
         'daiktai' => [
-            'telefonas' => [
-               'daikto_busena' => 'prapistas'
+            [
+                'pavadinimas' => 'telefonas',
+                'busena' => 'prapistas'
             ]
         ]
     ],
     '1' => [
         'vardas_pavarde' => 'Tomas Ablomas',
         'daiktai' => [
-            'buljonas' => [
-                'daikto_busena' => 'naudojamas'
+            [
+                'pavadinimas' => 'buljonas',
+                'busena' => 'naudojamas'
             ]
         ]
     ],
     '2' => [
         'vardas_pavarde' => 'Ana Shitkova',
         'daiktai' => [
-            'rulonas' => [
-                'daikto_busena' => 'pasibaiges'
+            [
+                'pavadinimas' => 'rulonas',
+                'busena' => 'pasibaiges'
             ],
         ]
     ]
