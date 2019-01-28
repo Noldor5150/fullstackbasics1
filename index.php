cde5f4c672aab1e253255acd58e15a180908b2e7
Noldor5150 <ruikis.paulius@gmail.com>
2019-01-28 12:30:04 +0200

    foreach 3

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 $mano_atmintis = ['penktadienis', 'paskaita', 'baras', 'alus', 'viskis', 'gintonikas', 'pirmadienis'];
+$draugo_atmintis = ['penktadienis', 'darbas', 'baras', 'alus', 'viskis', 'gintonikas', 'antradienis'];
 $atmintis = count($mano_atmintis);
 $random_flash = rand(0, $atmintis - 1);
 ?>
@@ -10,6 +11,12 @@ $random_flash = rand(0, $atmintis - 1);
     </head>
     <body>
         <h1>WTF?</h1>
+        <h2>Draugo atmintis</h2>
+        <ul>
+            <?php foreach ($draugo_atmintis as $value): ?>
+                <li><?php print $value; ?></li>
+            <?php endforeach; ?>
+        </ul> 
         <h2>Mano atmintis</h2>
         <h3><?php print $mano_atmintis[$random_flash]; ?></h3>
         <ul>
