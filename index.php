c3b51e30654e536844c1a4572f09a132b09256b3
Noldor5150 <ruikis.paulius@gmail.com>
2019-01-29 13:37:14 +0200

    ernesto uzdutis su procentais

--- index.php
+++ index.php
@@ -1,23 +1,42 @@
 <?php
-$rankinukas = [];
-$name = ['bambukas', 'pinigine', 'kremux', 'pirstines', 'baklazanas', 'dildo', 'tamponai', 'debilo_foto'];
+$name = [
+    'Slapios serveteles',
+    'Telefonas',
+    'Pinigine',
+    'Plyta',
+    'Elektrosokas',
+    'Duju balionelis',
+    'Belenkas',
+    'Baklazanas'
+];
 $rankinuko_dydis = rand(1, 20);
+$rankinuko_turis = 0;
 for ($i = 0; $i < $rankinuko_dydis; $i++) {
     $name_indexas = rand(0, count($name) - 1);
     $random_vardas = $name[$name_indexas];
     $size = rand(10, 50);
     $is_dark = rand(0, 1);
+    $rankinuko_turis += $size;
     if ($is_dark) {
-        $spalva = 'Sviesus';
-    } else {
         $spalva = 'Tamsus';
+    } else {
+        $spalva = 'Sviesus';
     }
     $rankinukas[] = [
         'name' => $random_vardas,
         'spalva' => $spalva,
+        'is_dark' => $is_dark,
         'size' => $size,
-        'info' => "$random_vardas Uzima: $size cm3. Daikto spalva: $spalva"
+        'info' => "$random_vardas Uzima: $size cm3. Daikto spalva: $spalva "
     ];
+}
+foreach ($rankinukas as &$value) {
+    $daikto_tikimybe = round($value['size'] / $rankinuko_turis * 100);
+    if ($value['is_dark']) {
+        $daikto_tikimybe /= 2;
+    }
+    $value['tikimybe'] = $daikto_tikimybe;
+    $value['info'] .= $daikto_tikimybe . '%';
 }
 ?>
 <html>
@@ -26,7 +45,7 @@ for ($i = 0; $i < $rankinuko_dydis; $i++) {
     </head>
     <body>
         <?php foreach ($rankinukas as $daiktas): ?>
-            <p><?php print $daiktas['info']; ?></p>
+            <p><?php print $daiktas['info'] ?></p>
         <?php endforeach; ?>
     </body>
 </html>
