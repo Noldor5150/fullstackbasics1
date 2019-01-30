038eb905a85231f6baf38b95d2c67561c4ad79b7
Noldor5150 <ruikis.paulius@gmail.com>
2019-01-30 12:07:45 +0200

    array ir for each kartojimas 3 uzduotis

--- index.php
+++ index.php
@@ -1,41 +1,31 @@
 <?php
-$balius = [
+$fiatas = [
     [
-        'pavadinimas' => 'Žuvis',
-        'kiekis' => 1,
-        'mato_vnt' => 'kibiras',
-        'kas_atnese' => 'Laura',
-        'stilius' => 'kibiras'
-    ],
-    [
-        'pavadinimas' => 'Baklažanai',
-        'kiekis' => 18,
-        'mato_vnt' => 'kg',
-        'kas_atnese' => 'Tomas',
-        'stilius' => 'kilogramai'
-    ],
-    [
-        'pavadinimas' => 'Aliejus',
-        'kiekis' => 0.7,
-        'mato_vnt' => 'litrai',
-        'kas_atnese' => 'Monika',
-        'stilius' => 'litrai'
-    ],
+        'gamintojas' => 'fiat',
+        'modelis' => '126p',
+        'metai' => [
+            'from' => 1972,
+            'to' => 2000
+        ],
+        'gamyklos' => [
+            'Kasinas',
+            'Termini Imereze',
+            'Kragujevacas',
+            'Belsko-Biala',
+            'Tychos'
+        ],
+        'klase' => 'Miesto automobilis',
+        'kebulo tips' => [
+            '2 duru sedanas',
+            '2 duru hecbekas'
+        ],
+        'variklis' => [
+            '594 cm3 2 cilindru',
+            '652 cm3 2 cilindru',
+            '704 cm3 2 cilindru'
+        ],
+        'dizaineris' => 'Sergio Sartorelli'
+    ]
 ];
-var_dump($balius);
+var_dump($fiatai);
 ?>
-<html>
-    <head>
-        <title>Balius</title>            
-        <link rel="stylesheet" href="css/main.css">
-    </head>
-    <body>
-        <div>
-            <?php foreach ($balius as $value): ?>
-                <p><?php print $value['kiekis'] ?>
-                   <span class="<?php print $value['stilius'] ?>"></span>
-                       <?php print $value['pavadinimas'] . '(' . $value['kas_atnese'] . ')'; ?></p>
-            <?php endforeach; ?>
-        </div>
-    </body>
-</html> 
