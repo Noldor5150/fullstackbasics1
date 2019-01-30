45022673369946da3ede8486a8432c466b0624bb
Noldor5150 <ruikis.paulius@gmail.com>
2019-01-30 19:52:57 +0200

    bandymas pakeisti zodi su kableliu tik i zodi kaip stringa , nepavyko

--- index.php
+++ index.php
@@ -1,21 +1,31 @@
 <?php
-$words = ['man', 'jei', 'daug', 'gersiu', 'Ona', 'neduos', 'bet', 'Aliona', 'duos', 'isvada', 'nori', 'nenori', 'negerk'];
-   $rasinys = ''; 
-while (strlen($rasinys) < 300) {
-    $kadataskas = rand(4, 7);
-    for ($i = 0; $i <= $kadataskas && strlen($rasinys) < 300; $i++) {
-        if ($i == 0) {
+$words = ['man', 'jei', 'daug', 'gersiu', 'Ona', 'neduos', ', bet', 'Aliona', 'duos', 'isvada', 'nori', 'nenori', 'negerk'];
+$rasinys = '';
+//$skirybos_zenklas = ['!', '.', '?'];
+while (strlen($rasinys) < 500) {
+    $kadataskas = rand(1, 2);
+    for ($i = 0; $i <= $kadataskas && strlen($rasinys) < 500; $i++) {
+        if ($i == 0 && $i === ',bet') {
+            $rasinys .= str_replace( ",bet", "Bet" ,$rasinys)  . $words[rand(0, count($words) - 1)] . ' ';
+        } else if ($i == 0) {
             $rasinys .= ucfirst($words[rand(0, count($words) - 1)]) . ' ';
         } else if ($i < $kadataskas) {
             $rasinys .= $words[rand(0, count($words) - 1)] . ' ';
         } else {
-            $rasinys .= $words[rand(0, count($words) - 1)] . '. ';
+            $rasinys .= $words[rand(0, count($words) - 1)] . '.' . ' ';//. $skirybos_zenklas [rand(0, count($skirybos_zenklas) - 1)] . ' ';
         }
-    };
+    }
 };
+
+str_replace('. , bet', '. Bet', $rasinys);
 $rasinys_simboliais = str_split($rasinys);
+
 $rasinys_simboliais[count($rasinys_simboliais) - 1] = '...';
 $output = implode($rasinys_simboliais) . ' (' . strlen($rasinys) . ')';
+
+//$ar = preg_split('/.,\s*/', $output, NULL, 
+     //   PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
+//var_dump($ar);
 ?>
 <html>
     <head>
