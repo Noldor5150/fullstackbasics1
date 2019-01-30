7a97e09b4da9822de35294c10bbd8f542a62df86
Noldor5150 <ruikis.paulius@gmail.com>
2019-01-30 11:48:20 +0200

    array kartojimas 2uzduotis

--- index.php
+++ index.php
@@ -1,24 +1,39 @@
 <?php
-
 $balius = [
-    'zuvis' => [
+    [
         'pavadinimas' => 'Žuvis',
         'kiekis' => 1,
         'mato_vnt' => 'kibiras',
         'kas_atnese' => 'Laura',
+        'stilius' => 'kibiras'
     ],
-    'baklazanas' => [
+    [
         'pavadinimas' => 'Baklažanai',
         'kiekis' => 18,
         'mato_vnt' => 'kg',
         'kas_atnese' => 'Tomas',
+        'stilius' => 'kilogramai'
     ],
-    'aliejus' => [
+    [
         'pavadinimas' => 'Aliejus',
         'kiekis' => 0.7,
         'mato_vnt' => 'litrai',
         'kas_atnese' => 'Monika',
+        'stilius' => 'litrai'
     ],
 ];
 var_dump($balius);
 ?>
+<html>
+    <head>
+        <title>Balius</title>            
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <div>
+            <?php foreach ($balius as $value): ?>
+            <p><?php print $value['kiekis'] ?> <span class="<?php print $value['stilius']?>"></span><?php print $value['pavadinimas'] . '(' . $value['kas_atnese'] . ')'; ?></p>
+            <?php endforeach; ?>
+        </div>
+    </body>
+</html> 
