7e7b3954c750146b0f7a06ac6871a323afffaa5f
Noldor5150 <ruikis.paulius@gmail.com>
2019-02-05 13:17:14 +0200

    5 uzduotis su refenrensais

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 
-$sheep = ['scheise', 'bliat', 'suca', 'subine', 'zanuda'];
-for ($i = 0; $i < 5; $i++) {
-    $sheep[$i] = &$sheep[0];
+$sheep = ['scheise'];
+for ($i = 1; $i < 5; $i++) {
+    $sheep[$i] = &$sheep[$i - 1];
 }
+foreach ($sheep as $key => $value) {
+    unset($sheep[$key]);
+    $sheep[$key] = $value;
+}
+$sheep[1] = 'sabaka';
 var_dump($sheep);
+?>
