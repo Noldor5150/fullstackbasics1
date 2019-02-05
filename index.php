9cdc6f3f39bf139756e6e12057e1e705ad544b81
Noldor5150 <ruikis.paulius@gmail.com>
2019-02-05 12:20:46 +0200

    trecia uzduotis su refenrensais

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-$x = 0;
-function change_x(&$x) {
-    $x = 5;
-}
-
-change_x($x);
-print $x; 
+$roll_joints = true;
+$joint1 = &$roll_joints;
+$joint2 = &$joint1;
+$joint3 = &$joint2;
+print $joint1;
+print $joint2;
+print $joint3;
 ?>
