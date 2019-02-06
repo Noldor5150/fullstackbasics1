7fc322f8d2cdcf7f2cb8403cbb3de25b5360b19d
Noldor5150 <ruikis.paulius@gmail.com>
2019-02-06 21:11:09 +0200

    namu darbai su POST, isprintinant inputus htmle

--- index.php
+++ index.php
@@ -1,9 +1,31 @@
 <?php
-$atsakymas = 0;
-if (isset($_POST['atsakymas'])) {
-    $atsakymas = $_POST['atsakymas'] + 1;
+$sertifikatas = '';
+$vardas = '';
+$pavarde = '';
+$amzius = '';
+$lygis = '';
+if (isset($_POST['vardas']) && isset($_POST['pavarde']) && isset($_POST['amzius']) && isset($_POST['lygis'])) {
+    $vardas = $_POST['vardas'];
+    $pavarde = $_POST['pavarde'];
+    $amzius = $_POST['amzius'];
+    $lygis = $_POST['lygis'];
+
+    if (!empty($vardas && $pavarde)) {
+        $vardas = $vardas;
+        $pavarde = $pavarde;
+        $amzius = $amzius;
+        $lygis = $lygis;
+        $sertifikatas = 'SERTIFIKATAS';
+    } else {
+        $vardas = 'daunas';
+        $pavarde = 'klaidu pridirbai';
+        $amzius = 'ilgai negyvensi';
+        $lygis = 'noobas';
+        $sertifikatas = 'BYBY tau o ne sertifikata, duxe';
+    }
 }
 ?>
+
 <html>
     <head>
         <title>About</title>
@@ -12,12 +34,20 @@ if (isset($_POST['atsakymas'])) {
     <body>
         <form method="POST">
             <div>
-                <input type="submit" name = "atsakymas" value="<?php print $atsakymas; ?>">
-                <?php for ($i = 0; $i < $atsakymas; $i++): ?>
-                    <img src="images/rain.gif" height="100" width="250">
-                <?php endfor; ?> 
+                <input type="text" name = "vardas" placeholder ="rašyk  savo vardą ,Duxas">
+                <input type="text" name = "pavarde" placeholder ="dabar pavardę, ple">
+                <input type="number" name = "amzius" placeholder ="kiek laiko Žemė nešioja?">
+                <select name="lygis">
+                    <option value ="Rookie" selected>Rookie</option>
+                    <option value ="Advanced">Advanced</option>
+                    <option value ="Prokofieff">Prokofieff</option>
+                </select>
+                <input type="submit" name = "submit" value="getcha PULL">
             </div>
         </form>
-        <h1>Atsakymas: <?php print $atsakymas; ?></h1>
+            <h1><?php print $sertifikatas; ?></h1>
+                <h2><?php print $vardas . ' ' . $pavarde; ?></h2>
+                   <p><?php print $amzius; ?></p>
+                   <p><?php print $lygis; ?></p>
     <body>
 </html>
