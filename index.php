e212b190df319b4739fd7eb0c29cd61a5cea2a64
Noldor5150 <ruikis.paulius@gmail.com>
2019-02-11 12:28:37 +0200

    forma is arrayforeach  metodai 1 su clean code

--- index.php
+++ index.php
@@ -2,18 +2,60 @@
 $input = filter_input_array(INPUT_POST, [
     'vardas' => FILTER_SANITIZE_SPECIAL_CHARS,
         ]);
+
+$array = [
+    'input' => [
+        'name' =>
+        [
+            'type' => 'text',
+            'placeholder' => 'Vardas',
+            'label' => 'Mano Vardas'
+        ],
+        'zirniai' =>
+        [
+            'type' => 'text',
+            'placeholder' => '1-100',
+            'label' => 'Kiek turiu zirniu?'
+        ],
+        'reason' =>
+        [
+            'placeholder' => 'issipasakok',
+            'label' => 'Paslaptis',
+            'type' => 'password',
+        ],
+    ],
+    'button' => [
+        'do_zirniai' =>
+        [
+            'label' => 'paberti'
+        ]
+    ]
+];
 ?>
 <html>
     <head>
-        <title>HACK ME - 02/11/2019</title>
-        <link rel="stylesheet" href="css/style.css">
+        <title>Forma is array</title>
+        <link rel="stylesheet" href="css/main.css">
     </head>
     <body>
-        <h1><?php print $input['vardas'] ?? ''; ?></h1>
-        <h2>Hack this page</h2>
         <form method="POST">
-            <input type="text" name="vardas">
-            <input type="submit">
+            <?php foreach ($array['input'] as $input_index => $input): ?>
+                <label>
+                    <span><?php print $input['label']; ?></span>
+                    <input type="<?php print $input['type']; ?>" 
+                           name="<?php print $input_index; ?>" 
+                           placeholder="<?php print $input['placeholder']; ?>">
+                </label>
+            <?php endforeach; ?>
+            <?php foreach ($array['button'] as $key => $value): ?>
+                <button
+                    name="action" 
+                    value="<?php print $key; ?>">
+                        <?php print $value['label']; ?>
+                </button>
+            <?php endforeach; ?>
         </form>
     </body>
 </html>
+
+   
