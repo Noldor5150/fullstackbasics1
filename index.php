dbd6a365e7beb56b1281aaa04e271e6b9b497ce9
Noldor5150 <ruikis.paulius@gmail.com>
2019-02-11 11:11:02 +0200

    saugumo metodai 1

--- index.php
+++ index.php
@@ -1,53 +1,18 @@
 <?php
-$sertifikatas = '';
-$vardas = '';
-$pavarde = '';
-$amzius = '';
-$lygis = '';
-if (isset($_POST['vardas']) && isset($_POST['pavarde']) && isset($_POST['amzius']) && isset($_POST['lygis'])) {
-    $vardas = $_POST['vardas'];
-    $pavarde = $_POST['pavarde'];
-    $amzius = $_POST['amzius'];
-    $lygis = $_POST['lygis'];
-
-    if (!empty($vardas && $pavarde)) {
-        $vardas = $vardas;
-        $pavarde = $pavarde;
-        $amzius = $amzius;
-        $lygis = $lygis;
-        $sertifikatas = 'SERTIFIKATAS';
-    } else {
-        $vardas = 'daunas';
-        $pavarde = 'klaidu pridirbai';
-        $amzius = 'ilgai negyvensi';
-        $lygis = 'noobas';
-        $sertifikatas = 'BYBY tau o ne sertifikata, duxe';
-    }
-}
+$_POST = filter_input_array(INPUT_POST,['vardas' =>FILTER_SANITIZE_SPECIAL_CHARS] );
 ?>
 
 <html>
     <head>
-        <title>About</title>
+        <title>Hack me</title>
         <link rel="stylesheet" href="css/main.css">
     </head>
     <body>
-        <form method="POST">
-            <div>
-                <input type="text" name = "vardas" placeholder ="rašyk  savo vardą ,Duxas">
-                <input type="text" name = "pavarde" placeholder ="dabar pavardę, ple">
-                <input type="number" name = "amzius" placeholder ="kiek laiko Žemė nešioja?">
-                <select name="lygis">
-                    <option value ="Rookie" selected>Rookie</option>
-                    <option value ="Advanced">Advanced</option>
-                    <option value ="Prokofieff">Prokofieff</option>
-                </select>
-                <input type="submit" name = "submit" value="getcha PULL">
-            </div>
+        <h1><?php print $_POST['vardas']?? '';?></h1>
+        <h2> hack this page</h2>
+                <form method="POST">
+            <input type="text" name = "vardas">
+            <input type="submit">
         </form>
-            <h1><?php print $sertifikatas; ?></h1>
-                <h2><?php print $vardas . ' ' . $pavarde; ?></h2>
-                   <p><?php print $amzius; ?></p>
-                   <p><?php print $lygis; ?></p>
     <body>
 </html>
